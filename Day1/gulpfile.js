36ebab5afd0c491ec9afed7abe6e1a928d403238
Franchette Camoro <nicolecamoro@gmail.com>
2016-12-30 19:39:59 +0800

    First commit

---
+++ Day1/gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var autoPrefixer = require('gulp-autoprefixer');
+var cleanCss = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var server = require('gulp-server-livereload');
+
+
+gulp.task('webserver', function() {
+  gulp.src('.')
+    .pipe(server({
+      livereload: true,
+      directoryListing: true,
+      open: true
+    }));
+});
+
+gulp.task('sass',function(){
+    gulp.src(['src/styles/**/*.sass'])
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(autoPrefixer())
+        .pipe(gulp.dest('public/css/'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(cleanCss())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('public/css/'))
+});
+
+
+gulp.task('js',function(){
+    gulp.src(['src/js/**/*.js'])
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('public/src'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/src'))
+});
+
+
+gulp.task('default',function(){
+    gulp.watch('src/js/**/*.js',['js']);
+    gulp.watch('src/styles/**/*.sass',['sass']);
+});
